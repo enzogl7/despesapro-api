a404ad1df90defea0e960603f9ab289634db061e
enzogl7 <enzolima527@gmail.com>
2025-08-04 08:08:57 -0300

    Initial commit

---
+++ src/main/java/com/ogl/despesapro_api/DespesaproApiApplication.java
@@ -0,0 +1,13 @@
+package com.ogl.despesapro_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespesaproApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespesaproApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ogl/despesapro_api/DespesaproApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogl.despesapro_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespesaproApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
